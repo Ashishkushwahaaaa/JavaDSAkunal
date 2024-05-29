8b8873ded245df34e06e9bc503c79003ad447506
Ashishkushwahaaaa <ashishkushwahaaaa@gmail.com>
2024-05-29 23:28:23 +0530

    Matrix Binary Search

--- Binary_Search/MatrixBinarySearch.java
+++ Binary_Search/MatrixBinarySearch.java
@@ -1,17 +1,29 @@
 package Binary_Search;
-
+import java.util.Arrays;
 public class MatrixBinarySearch {
     public static void main(String[] args) {
-        int [][] arr= {
+        int [][] matrix= {
             {10,20,30,40},
             {15,25,35,45},
             {28,29,37,47},
             {33,34,38,50}
         };
-
-        int c = arr.length;
-        System.out.println(c);
+        System.out.println(Arrays.toString(Search(matrix,34)));
     }
-    
+    static int[] Search(int[][] matrix, int target){
+        int r=0;
+        int c=matrix.length-1;
+        while (r<matrix.length && c>=0) {
+            if (matrix[r][c]==target) {
+                return new int[] {r,c};
+            }
+            if (matrix[r][c]<target) {
+                r++;
+            }else{
+                c--;
+            }
+        }
+        return new int[] {-1,-1};
+    }
             
 }
