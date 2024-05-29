0c60ea559b91f81e9afda33eddcbef3748d2c723
Ashishkushwahaaaa <ashishkushwahaaaa@gmail.com>
2024-05-29 23:08:07 +0530

    Rename

---
+++ Binary_Search/MatrixBinarySearch.java
@@ -0,0 +1,17 @@
+package Binary_Search;
+
+public class MatrixBinarySearch {
+    public static void main(String[] args) {
+        int [][] arr= {
+            {10,20,30,40},
+            {15,25,35,45},
+            {28,29,37,47},
+            {33,34,38,50}
+        };
+
+        int c = arr.length;
+        System.out.println(c);
+    }
+    
+            
+}
