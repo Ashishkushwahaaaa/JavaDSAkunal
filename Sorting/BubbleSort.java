ac75b6ad3a8c6689b21619b79ed26ab10915d9fe
Ashishkushwahaaaa <ashishkushwahaaaa@gmail.com>
2024-05-31 15:59:53 +0530

    SelectionSort Algorithm

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -3,16 +3,15 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        // int[] arr = {2,0,1,-3,4,6,3,1,7,5};
-        int[] arr = {1,2,3,4,5,6};
-
+        int[] arr = {2,0,1,-3,4,6,3,1,7,5};
+        // int[] arr = {1,2,3,4,5,6};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void bubble(int[] arr){
         for(int i = 0; i < arr.length; i++){
             boolean swapped = false;
-            for (int j = 1; j < arr.length-i; j++) {
+            for (int j = 1; j < arr.length-i; j++) { // arr.length-i because at each pass one element (largest) will be at it's right place;
                 if(arr[j]<arr[j-1]){
                     //than swap
                     int temp = arr[j];
@@ -21,6 +20,10 @@ public class BubbleSort {
                     swapped=true;
                 }
             }
+
+            //if the array is already sorted than we need not to check for all the value of i
+            //if for no one values of j during any pass, the swapping is not done that's mean now all the element are sorted i.e array is
+            //sorted so need to check further for the other values of i. 
             if (!swapped) {
                 break;
             }
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        // int[] arr = { 2, 4, 1, 6, 3, 7, 5 };
+        int[] arr = { 2, 4};
+        Selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void Selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length-i-1;
+            int max = findMaxIndex(arr, 0, last);
+            swap(arr,max,last);
+        }
+    }
+    static int findMaxIndex(int[] arr, int start, int end){
+        int max =start;
+        for (int i = start; i <=end; i++) {
+            if (arr[max]<arr[i]) {
+                max=i;
+            }
+        }
+        return max;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
