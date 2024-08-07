fef6aa3175297b7a72795d16c09a38e3cb8eeed4
Ashishkushwahaaaa <ashishkushwahaaaa@gmail.com>
2024-08-08 01:20:14 +0530

    Solving easy level of recursion

---
+++ Recursion/DigitProduct.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class DigitProduct {
+    public static void main(String[] args) {
+        int n = 3043; //If contains zero then the answer will be zero.
+        System.out.println(product(n));
+    }
+    static int product(int n){
+        if (n%10 == n) { // This to get the last digit and then return the last value rather than to wait n to be zero and then return likewise in digitSum which will lead to result = 0 always
+            return n;
+        }
+        return n%10 * product(n/10);
+    }
+}
---
+++ Recursion/ReverseNum.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        
+    }
+    static int RevNum(int n){
+        return 
+    }
+}
