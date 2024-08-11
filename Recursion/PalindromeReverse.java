440c9a3dc72106579c91ec262c0b8021d9677583
Ashishkushwahaaaa <ashishkushwahaaaa@gmail.com>
2024-08-11 19:00:14 +0530

    Reverse of a number and hence palindrome

---
+++ Recursion/PalindromeReverse.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class PalindromeReverse {
+    public static void main(String[] args) {
+        int n = 121;
+        // System.out.println(Rev(n));
+        System.out.println(Palindrome(n));
+    }
+
+    private static boolean Palindrome(int n){
+        return (n==Rev(n));
+    }
+
+    private static int Rev(int n){
+        int digit = (int)Math.log10(n)+1;
+        return helper(n, digit);
+    }
+    private static int helper(int n, int digit){
+        if (n%10==n) {
+            return n;
+        }
+        return n%10*(int)Math.pow(10, digit-1)+helper(n/10, digit-1);
+    }
+}
--- Recursion/ReverseNum.java
+++ Recursion/ReverseNum.java
@@ -1,10 +1,30 @@
 package Recursion;
 
 public class ReverseNum {
+
+    static int sum = 0;
     public static void main(String[] args) {
-        
+        Rev1(12345);        
+        System.out.println(sum);
+        System.out.println(Rev2(12345));
     }
-    static int RevNum(int n){
-        return 
+    private static void Rev1(int num){
+        if (num==0) {
+            return;
+        }
+        int rem = num%10;
+        sum=sum*10+rem;
+        Rev1(num/10);
+    }
+    private static int Rev2(int n){
+        // When we don't want to use an extra variable(like above we made static variable sum), then we can create an helper function 
+        int digits = (int)Math.log10(n)+1;
+        return helper(n,digits);
+    }
+    private static int helper(int n, int digits) {
+        if (n%10==n) {
+            return n;
+        }
+        return n%10*(int)Math.pow(10,digits-1)+helper(n/10, digits-1);
     }
 }
